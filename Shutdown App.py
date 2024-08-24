135c3f329f80dd3752b1e182930b71ac5c7a0634
Aditya Goswami <aditya.goswami2025@gmail.com>
2024-08-24 07:21:43 +0530

    ShutDown App

--- Shutdown App.py
+++ Shutdown App.py
@@ -15,16 +15,16 @@ st.title("ShutDown App")    # Giving a Title
 st.geometry("500x500")  # Dimensions
 st.config(bg="medium spring green")
 
-r_button = Button(st, text = "Restart", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command=restart())   # Creating Restart Button
+r_button = Button(st, text = "Restart", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command=restart)   # Creating Restart Button
 r_button.place(x=150, y=60, height=50, width=200)    # Height & Width
 
-rt_button = Button(st, text = "Restart Time", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command=restart_time())   # Creating Restart Button
+rt_button = Button(st, text = "Restart Time", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command=restart_time)   # Creating Restart Button
 rt_button.place(x=150, y=170, height=50, width=200)    # Height & Width
 
-lg_button = Button(st, text = "Log-Out", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command = logout())   # Creating Restart Button
+lg_button = Button(st, text = "Log-Out", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command = logout)   # Creating Restart Button
 lg_button.place(x=150, y=270, height=50, width=200)    # Height & Width
 
-st_button = Button(st, text = "ShutDown", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command = shutdown())   # Creating Restart Button
+st_button = Button(st, text = "ShutDown", font = ("ROBOTO", 20, "bold"), relief = RAISED, cursor = "plus", command = shutdown)   # Creating Restart Button
 st_button.place(x=150, y=370, height=50, width=200)    # Height & Width
 
 st.mainloop()
