a1906558aefa4ba1dfb24e0b23d5078286f0e6b5
Aditya Goswami <aditya.goswami2025@gmail.com>
2024-08-24 06:39:07 +0530

    QR code

--- QR Code Generator/QR Code formatting.py
+++ QR Code Generator/QR Code formatting.py
@@ -1,4 +1,12 @@
 import qrcode
 from PIL import Image
+import qrcode.constants
 
-qr = qrcode.QRCode
+qr = qrcode.QRCode(version=1, 
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=15,
+                   border=10,)
+qr.add_data("https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools.html")
+qr.make(fit=True)
+img = qr.make_image(fill_color="green",back_color="white")
+img.save("AMD Vivado ML Download.png")
